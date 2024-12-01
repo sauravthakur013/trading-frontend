8733638822b15abc863a65280ad58da3b39e00c2
SauravBhooriya <sauravthakur00013jan@gmail.com>
2024-12-01 19:35:35 +0530

    api setup

--- src/communication/constants.ts
+++ src/communication/constants.ts
@@ -1,4 +1,4 @@
-export const PROD_API = 'https://trading-1-qymd.onrender.com/'; 
+export const PROD_API = 'https://trading-1-qymd.onrender.com/api'; 
 export const DEV_API = 'http://localhost:4040/api/';
 
 export const DASH_API_DEV = 'http://localhost:3000/';
